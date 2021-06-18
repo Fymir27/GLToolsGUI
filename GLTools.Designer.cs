694134a7e0bea38de202c7654128ab40f3323eae
Fymir27 <oliver.poeckelhofer@student.tugraz.at>
2021-06-18 17:46:33 +0200

    first commit

---
+++ GLTools.Designer.cs
@@ -0,0 +1,189 @@
+﻿
+namespace GLToolsGUI
+{
+    partial class GLTools
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.openKTEX = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.savePNG = new System.Windows.Forms.Button();
+            this.loadedKTEXPreview = new System.Windows.Forms.PictureBox();
+            this.saveKTEX = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.openImage = new System.Windows.Forms.Button();
+            this.kleiMagicBytesPreview = new System.Windows.Forms.Label();
+            this.loadedImagePreview = new System.Windows.Forms.PictureBox();
+            this.errorText = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.loadedKTEXPreview)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.loadedImagePreview)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // openKTEX
+            // 
+            this.openKTEX.Location = new System.Drawing.Point(82, 8);
+            this.openKTEX.Name = "openKTEX";
+            this.openKTEX.Size = new System.Drawing.Size(75, 23);
+            this.openKTEX.TabIndex = 3;
+            this.openKTEX.Text = "Choose File";
+            this.openKTEX.UseVisualStyleBackColor = true;
+            this.openKTEX.Click += new System.EventHandler(this.OpenKTEX_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Klei Texture";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // savePNG
+            // 
+            this.savePNG.Location = new System.Drawing.Point(163, 8);
+            this.savePNG.Name = "savePNG";
+            this.savePNG.Size = new System.Drawing.Size(90, 23);
+            this.savePNG.TabIndex = 5;
+            this.savePNG.Text = "Save as PNG";
+            this.savePNG.UseVisualStyleBackColor = true;
+            this.savePNG.Click += new System.EventHandler(this.SavePNG_Click);
+            // 
+            // loadedKTEXPreview
+            // 
+            this.loadedKTEXPreview.Location = new System.Drawing.Point(12, 37);
+            this.loadedKTEXPreview.Name = "loadedKTEXPreview";
+            this.loadedKTEXPreview.Size = new System.Drawing.Size(381, 401);
+            this.loadedKTEXPreview.TabIndex = 6;
+            this.loadedKTEXPreview.TabStop = false;
+            this.loadedKTEXPreview.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // saveKTEX
+            // 
+            this.saveKTEX.Location = new System.Drawing.Point(658, 8);
+            this.saveKTEX.Name = "saveKTEX";
+            this.saveKTEX.Size = new System.Drawing.Size(130, 23);
+            this.saveKTEX.TabIndex = 9;
+            this.saveKTEX.Text = "Save as Klei Texture";
+            this.saveKTEX.UseVisualStyleBackColor = true;
+            this.saveKTEX.Click += new System.EventHandler(this.saveKTEX_click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(489, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(82, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Standard Image";
+            // 
+            // openImage
+            // 
+            this.openImage.Location = new System.Drawing.Point(577, 8);
+            this.openImage.Name = "openImage";
+            this.openImage.Size = new System.Drawing.Size(75, 23);
+            this.openImage.TabIndex = 7;
+            this.openImage.Text = "Choose File";
+            this.openImage.UseVisualStyleBackColor = true;
+            this.openImage.Click += new System.EventHandler(this.openImage_Click);
+            // 
+            // kleiMagicBytesPreview
+            // 
+            this.kleiMagicBytesPreview.AutoSize = true;
+            this.kleiMagicBytesPreview.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.kleiMagicBytesPreview.Location = new System.Drawing.Point(259, 13);
+            this.kleiMagicBytesPreview.Name = "kleiMagicBytesPreview";
+            this.kleiMagicBytesPreview.Size = new System.Drawing.Size(134, 13);
+            this.kleiMagicBytesPreview.TabIndex = 10;
+            this.kleiMagicBytesPreview.Text = "0x69 0x69 0x69 0x69 0x69";
+            this.kleiMagicBytesPreview.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.kleiMagicBytesPreview.Click += new System.EventHandler(this.kleiMagicBytesPreview_Click);
+            // 
+            // loadedImagePreview
+            // 
+            this.loadedImagePreview.Location = new System.Drawing.Point(407, 37);
+            this.loadedImagePreview.Name = "loadedImagePreview";
+            this.loadedImagePreview.Size = new System.Drawing.Size(381, 401);
+            this.loadedImagePreview.TabIndex = 11;
+            this.loadedImagePreview.TabStop = false;
+            // 
+            // errorText
+            // 
+            this.errorText.AutoSize = true;
+            this.errorText.ForeColor = System.Drawing.Color.Red;
+            this.errorText.Location = new System.Drawing.Point(12, 441);
+            this.errorText.Name = "errorText";
+            this.errorText.Size = new System.Drawing.Size(74, 13);
+            this.errorText.TabIndex = 12;
+            this.errorText.Text = "No Errors (yet)";
+            // 
+            // GLTools
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 460);
+            this.Controls.Add(this.errorText);
+            this.Controls.Add(this.loadedImagePreview);
+            this.Controls.Add(this.kleiMagicBytesPreview);
+            this.Controls.Add(this.saveKTEX);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.openImage);
+            this.Controls.Add(this.loadedKTEXPreview);
+            this.Controls.Add(this.savePNG);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.openKTEX);
+            this.Name = "GLTools";
+            this.Text = "GLTools";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.loadedKTEXPreview)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.loadedImagePreview)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button openKTEX;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button savePNG;
+        private System.Windows.Forms.PictureBox loadedKTEXPreview;
+        private System.Windows.Forms.Button saveKTEX;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button openImage;
+        private System.Windows.Forms.Label kleiMagicBytesPreview;
+        private System.Windows.Forms.PictureBox loadedImagePreview;
+        private System.Windows.Forms.Label errorText;
+    }
+}
+
---
+++ GLTools.cs
@@ -0,0 +1,221 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ImageMagick;
+
+namespace GLToolsGUI
+{
+    public partial class GLTools : Form
+    {
+        private string[] allowedImageTypes = new[]
+        {
+            "png",
+            "jpg",
+            "jpeg",
+            "gif"
+        };
+
+        MagickImage currentlyLoadedImage;
+        MagickImage currentlyLoadedKTEX;
+        const int magicBytesCount = 5;
+        byte[] kleiMagicBytes = null;
+        byte[] kleiFileMagic = { (byte)'K', (byte)'T', (byte)'E', (byte)'X' };
+
+        public GLTools()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            kleiMagicBytesPreview.Text = "No original texture loaded";
+        }       
+
+        private void SetLoadedKTEX(MagickImage image)
+        {
+            currentlyLoadedKTEX = image;
+            var previewImage = currentlyLoadedKTEX.Clone();
+            previewImage.Resize(loadedKTEXPreview.Width, loadedKTEXPreview.Height);
+            loadedKTEXPreview.Image = Image.FromStream(new MemoryStream(previewImage.ToByteArray(MagickFormat.Png)));
+        } 
+        
+        private void SetLoadedImage(MagickImage image)
+        {
+            currentlyLoadedImage = image;
+            var previewImage = currentlyLoadedImage.Clone();
+            previewImage.Resize(loadedImagePreview.Width, loadedKTEXPreview.Height);
+            loadedImagePreview.Image = Image.FromStream(new MemoryStream(previewImage.ToByteArray(MagickFormat.Png)));
+        }
+
+        private string bytesToString(byte[] bytes)
+        {
+            return bytes.Aggregate("", (carry, currentByte) => carry += (char)currentByte);
+        }
+
+        private void OpenKTEX_Click(object sender, EventArgs e)
+        {
+            using (var dialog = new OpenFileDialog())
+            {
+                var fileTypeString = string.Join(";", allowedImageTypes.Select(type => $"*.{type}"));
+                dialog.InitialDirectory = Directory.GetCurrentDirectory();
+                dialog.Filter = "Klei Texture (*.tex)|*.tex";                
+                dialog.RestoreDirectory = true;
+                if(dialog.ShowDialog() != DialogResult.OK)
+                {
+                    errorText.Text = "Error selecting file";
+                    return;
+                }
+
+                var fs = dialog.OpenFile();
+                int magicLength = 4;
+                var magic = new byte[magicLength];                
+                if(fs.Read(magic, 0, magicLength) < magicLength)
+                {
+                    errorText.Text = "Failed to read magic!";
+                    return;
+                }
+
+                var magicString = bytesToString(magic);
+
+                if(magicString == "KTEX")
+                {                    
+                    kleiMagicBytes = new byte[magicBytesCount];
+                    if (fs.Read(kleiMagicBytes, 0, magicBytesCount) < magicBytesCount)
+                    {
+                        errorText.Text = "Failed to read KLEI disrcriminator!";
+                        return;
+                    }
+                    kleiMagicBytesPreview.Text = "";
+                    foreach (var b in kleiMagicBytes)
+                    {
+                        kleiMagicBytesPreview.Text += (int)b;
+                    }
+                } 
+                else if(magicString == "DDS")
+                {
+                    fs.Position = 0;
+                } 
+                else
+                {
+                    errorText.Text = "Invalid file magic";
+                    return;
+                }
+
+                long bytesLeft = fs.Length - fs.Position;
+                var ddsImageBytes = new byte[bytesLeft];                
+                fs.Read(ddsImageBytes, 0, (int)bytesLeft);
+                fs.Close();
+
+                SetLoadedKTEX(new MagickImage(ddsImageBytes));
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void SavePNG_Click(object sender, EventArgs e)
+        {
+            if (currentlyLoadedKTEX == null)
+            {
+                errorText.Text = "No image loaded";
+                return;
+            }
+
+            using (var dialog = new SaveFileDialog())
+            {
+                dialog.InitialDirectory = Directory.GetCurrentDirectory();
+                dialog.Filter = "PNG Image (*.png)|*.png";
+                dialog.RestoreDirectory = true;
+
+                if (dialog.ShowDialog() != DialogResult.OK)
+                {
+                    errorText.Text = "Error selecting file";
+                    return;
+                }
+
+                currentlyLoadedKTEX.Write(dialog.FileName);
+            }   
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void openImage_Click(object sender, EventArgs e)
+        {
+            using (var dialog = new OpenFileDialog())
+            {
+                var fileTypeString = string.Join(";", allowedImageTypes.Select(type => $"*.{type}"));
+                dialog.InitialDirectory = Directory.GetCurrentDirectory();
+                dialog.Filter = "Image (*.png,*.jpeg)|*.png;*jpeg";
+                dialog.RestoreDirectory = true;
+                if (dialog.ShowDialog() != DialogResult.OK)
+                {
+                    errorText.Text = "Error selecting file";
+                    return;
+                }
+
+                SetLoadedImage(new MagickImage(dialog.FileName));
+            }
+        }
+
+        private void kleiMagicBytesPreview_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void saveKTEX_click(object sender, EventArgs e)
+        {
+            if (currentlyLoadedImage == null)
+            {
+                errorText.Text = "No image loaded";
+                return;
+            }
+
+            if (kleiMagicBytes == null)
+            {
+                errorText.Text = "Please load original texture first";
+                return;
+            }
+
+            using (var dialog = new SaveFileDialog())
+            {
+                dialog.InitialDirectory = Directory.GetCurrentDirectory();
+                dialog.Filter = "Klei Texture (*.tex)|*.tex";
+                dialog.RestoreDirectory = true;
+
+                if (dialog.ShowDialog() != DialogResult.OK)
+                {
+                    errorText.Text = "Error selecting file";
+                    return;
+                }
+
+                currentlyLoadedImage.Format = MagickFormat.Dds;
+                currentlyLoadedImage.Settings.SetDefine(MagickFormat.Dds, "compression", "dxt5");
+                var rawDDS = currentlyLoadedImage.ToByteArray();
+
+                var outputFile = File.OpenWrite(dialog.FileName);
+
+                outputFile.Write(kleiFileMagic, 0, kleiFileMagic.Length);
+                outputFile.Write(kleiMagicBytes, 0, magicBytesCount);
+                outputFile.Write(rawDDS, 0, rawDDS.Length);
+                outputFile.Close();
+            }
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+    }
+}
